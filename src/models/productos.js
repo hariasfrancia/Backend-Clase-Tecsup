b72a95d7309cc27fd9ab3323f068b8ee6162f679
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-04-23 19:13:01 -0500

    creando la carpeta models, y el archivo productos.js, se esta creando los modelos de la BD

---
+++ src/models/productos.js
@@ -0,0 +1,25 @@
+// DAtatypes => https://sequelize.org/master/manual/model-basics.html#data-types
+import { DataTypes } from "sequelize";
+import { conexion } from "../config/sequelize";
+
+// Opciones para poner a la columnas: https://sequelize.org/master/manual/model-basics.html#column-options
+
+export default producto_model = () => conexion.define("producto",{
+    productoId: {
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        unique:true,
+        autoIncrement: true,
+        field: 'id',
+    },
+    productoNombre: {
+        type: DataTypes,stringify(45),
+        field: "nombre",
+    },
+    productoPrecio: {
+        type: DataTypes.DECIMAL(5, 2),
+        field: "precio",
+    },
+});
+
