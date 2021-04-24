9abd87287a3eb2d212ed2f33170482c6dcd05572
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-04-23 22:35:50 -0500

    agregando return

--- src/controllers/producto.js
+++ src/controllers/producto.js
@@ -5,7 +5,7 @@ export const crearProducto = (req, res) => {
     // crear nuevo producto, las conexiones con la base de datos son asuincronas, usamos await
     // Cuando creamos un nuevo registro, este retornara el registro creado en la base datos
     const nuevoProducto = await Producto.create(req, body);
-    res.status(201).json({
+    return res.status(201).json({
         success: true,
         content: nuevoProducto,
         message: "Producto creado exitosamente",
