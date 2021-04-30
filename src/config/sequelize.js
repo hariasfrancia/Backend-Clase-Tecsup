22dc7c3baf4269d3ebca5fc7f7eb3ab2e96e06d6
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-04-29 19:42:12 -0500

    modifique cadena de conexion en el sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,20 +1,46 @@
 import { Sequelize } from "sequelize";
 // creamos una variable
-export const conexion = new Sequelize(
-    "almacen",//database name
-    "root", //username
-    "root", // password
-    {
-        dialect: "mysql", // tambien podemos usar dialectos para pgadmin,sqlserver,sqlite3,mariaadb,mysql 
-        // /*one of 'mysql | 'mariadb' | postgres 'mysql */ para sqllite se usa conexion URI
-        host: "127.0.0.1",
-        port: 3306,
-        timezone: "-05:00", //no funciona en SQLITE
+export const conexion = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL, {
+        dialect: "mysql",
+        timezone: "-05:00",
+        logging: false,
         dialectOptions: {
-            //sirve para que al momento de mostrar las fechgas, automaticamente las convierta a string y no tener que hacer una converison manual
             dateStrings: true,
         },
-        // logging: true,
-        logging: false,
-    }
-);
+    })
+    : new Sequelize(
+        "almacen", // database name
+        "root", // username
+        "root", // password
+        {
+            dialect: "mysql", // tambien podemos usar dialectos para pgadmin, sqlserver, sqlite3, mariadb, mysql
+            // /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */ para sqlite se usa connection URI
+            host: "127.0.0.1",
+            port: 3306,
+            timezone: "-05:00", // no funciona en SQLITE
+            dialectOptions: {
+                // sirve para que al momento de mostrar las fechas, automaticamente las convierta en string y no tener que hacer una conversion manual
+                dateStrings: true,
+            },
+            logging: false,
+        }
+    );
+// export const conexion = process.env.JAWSDB_URL ? new Sequelize new Sequelize(
+//     "almacen",//database name
+//     "root", //username
+//     "root", // password
+//     {
+//         dialect: "mysql", // tambien podemos usar dialectos para pgadmin,sqlserver,sqlite3,mariaadb,mysql 
+//         // /*one of 'mysql | 'mariadb' | postgres 'mysql */ para sqllite se usa conexion URI
+//         host: "127.0.0.1",
+//         port: 3306,
+//         timezone: "-05:00", //no funciona en SQLITE
+//         dialectOptions: {
+//             //sirve para que al momento de mostrar las fechgas, automaticamente las convierta a string y no tener que hacer una converison manual
+//             dateStrings: true,
+//         },
+//         // logging: true,
+//         logging: false,
+//     }
+// );
