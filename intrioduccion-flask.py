74b0ab09d984c851dc3c689e1fac002e66623a74
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-05-16 19:47:14 -0500

    cargando el archivo introduccion-flask

---
+++ intrioduccion-flask.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import flask;
+
+#! la variable app sera la instancia de la clase Flask
+#! el primer parametro para el contrscutor de la clase Flask tiene que ser el hilo en el cual se esta ejecutando la aplicacion (en el archivo principal de nuestro proyecto)
+#! __name__ sircve para definir que nuestra clase aplicacion de flask se va a ejecutar en el archivo principal del proyecto
+app = Flask(__name__)
+
+# Usamos un decorador, def una funcion
+# Es un controlador que se va ejecutar cuando se llame la ruta 127.0.0.1:5000/
+@app.route('/')
+def ruta_inicial():
+    return 'El servidor se ha levantado exitosamente.'
+
+app.run(debug=True)
