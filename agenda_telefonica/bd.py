e19a4989e7c85563f322b8b7bef29cb8e84db34e
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-05-20 18:39:02 -0500

    limpiando la rama

--- agenda_telefonica/bd.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-bd = SQLAlchemy()
-
-# ? Hacer el MER de uan base datos para una agenda telefonica, en la cual mediante un nombre que peuda tener varios numeros telefonicos, inidcando el tipo de numero (casa, oficina, personal, fax)
