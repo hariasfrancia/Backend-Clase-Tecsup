a82f87bddfa42f32607ca64e3bffb02f393bd048
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-05-22 20:40:55 -0500

    actualizando proyectos

---
+++ paint-con-bd/frontend/index.js
@@ -0,0 +1,38 @@
+const socket = io("http://127.0.0.1:5000");
+// setup => funcion que configura el canvas con dimensiones y color de fondo
+function setup() {
+    createCanvas(300, 300);
+    background(100);
+    socket.on("coordenadas", (data) => {
+        data.forEach((registro) => {
+            registro.coordenadas.forEach((coordenada) => {
+                fill(registro.color);
+                noStroke();
+                ellipse(
+                    coordenada.x.$numberDecimal,
+                    coordenada.y.$numberDecimal,
+                    15,
+                    15
+                );
+            });
+        });
+    });
+}
+// mouseDragged => solamente se pintara cuando hagamos click
+// draw => basta con pasar el cursor por el canvas
+function mouseDragged() {
+    fill("#70EF41");
+    noStroke(); // sirve para no bordear el lienzo
+    ellipse(mouseX, mouseY, 15, 15);
+    enviarPunto(mouseX, mouseY);
+}
+
+const enviarPunto = (posX, posY) => {
+    // console.log(`Posicion x: ${posX}`);
+    // console.log(`Posicion Y: ${posY}`);
+    const data = {
+        x: posX,
+        y: posY,
+    };
+    socket.emit("coordenada", data);
+};
--- paint-sin-bd/frontend/pinturillo.js
+++ paint-sin-bd/frontend/pinturillo.js
@@ -4,32 +4,23 @@ function setup() {
     createCanvas(300, 300);
     background(100);
     socket.on("coordenadas", (data) => {
-        data.forEach((registro) => {
-            registro.coordenadas.forEach((coordenada) => {
-                fill(registro.color);
-                noStroke();
-                ellipse(
-                    coordenada.x.$numberDecimal,
-                    coordenada.y.$numberDecimal,
-                    15,
-                    15
-                );
-            });
-        });
+        fill(0, 0, 255);
+        noStroke();
+        ellipse(data.x, data.y, 15, 15);
     });
 }
 // mouseDragged => solamente se pintara cuando hagamos click
 // draw => basta con pasar el cursor por el canvas
 function mouseDragged() {
-    fill("#00000");
+    fill(112, 239, 65);
     noStroke(); // sirve para no bordear el lienzo
     ellipse(mouseX, mouseY, 15, 15);
     enviarPunto(mouseX, mouseY);
 }
 
 const enviarPunto = (posX, posY) => {
-    // console.log(`Posicion x: ${posX}`);
-    // console.log(`Posicion Y: ${posY}`);
+    console.log(`Posicion x: ${posX}`);
+    console.log(`Posicion Y: ${posY}`);
     const data = {
         x: posX,
         y: posY,
